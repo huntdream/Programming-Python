c0f5dffe8fa642f3032993a20db1b521e49610fe
Yu <maoyu960320@hotmail.com>
2017-04-21 23:11:57 +0800

    Add some code

--- basic_concept.py
+++ basic_concept.py
@@ -10,3 +10,20 @@ print(my_name)
 age = map((lambda x: x[2]), about_me)
 print(list(age))
 
+about_me.append(['You', 'And', 'Me'])
+
+print(len(about_me))
+
+name, sex, age = range(3)
+
+print(me[name], me[sex], me[age])
+
+# Dictionaries
+me = {'name': 'Yu Mao', 'sex': 'Male', 'age': '21'}
+you = dict(name='Life', sex='Dream', age='Future')
+nothing = {}
+nothing['You'] = 'Nothing'
+nothing['And'] = 'Is'
+nothing['Me'] = "True"
+
+print(you, me, nothing, sep='\n')
